8d3587ecc959320c4609894ff39882fda0d3c4fd
108021062 <108021062@live.asia.edu.tw>
2019-10-30 19:07:37 +0800

    考試測驗

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
+        int n=sc.nextInt();
+        for (int i=1;i<=n;i++){
+            int pass=0,total=0,m=0;
+            for (int j=1;j<=3;j++) {
+                int n1 = sc.nextInt();
+                if (n1>=60){
+                    pass++;
+                }
+                total+=n1;
+                m=Math.max(m,n1);
 
-        int gcd=0;
-        for (int i=1;i<=a;i++) {
-            if (a % i == 0&&b%i==0) {
-                gcd=i;
+            }if (pass==3){
+                System.out.println("P");
+            }else if (pass==2&&total>=220){
+                System.out.println("P");
+            }else if (pass==2){
+                System.out.println("M");
+            }else if (m>=80){
+                System.out.println("M");
+            }else{
+                System.out.println("F");
             }
         }
-        System.out.println(gcd);
-        System.out.println(a*b/gcd);
-        }
     }
+}
+
 
 
